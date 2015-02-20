0b63abd894098eafef7bcfbdbed7dba94f52172b
MartinCazares <jm.cazaresg@gmail.com>
2015-02-19 23:13:06 -0800

    Adding debug project into git...

---
+++ MacroDebug/app/debugger_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ $1 = 'release' ]; then
+	find ./src/main/java -name "*.java" -exec \
+	sed -i '' -e 's/\/\/<#DEBUG>/\/\*<#DEBUG_OFF>/g' -e 's/\/\/<\/#DEBUG>/<\/#DEBUG_OFF>\*\//g' {} +
+	echo "DEBUGGER SCRIPT: RELEASE MODE"
+fi
+
+if [ $1 = 'debug' ]; then
+	find ./src/main/java -name "*.java" -exec \
+	sed -i '' -e 's/\/\*<#DEBUG_OFF>/\/\/<#DEBUG>/g' -e 's/<\/#DEBUG_OFF>\*\//\/\/<\/#DEBUG>/g' {} +
+	echo "DEBUGGER SCRIPT: DEBUG MODE"
+fi
+
+echo "DEBIGGER SCRIPT: DONE EDITING"
